ad6f4eacdba7c42667b09fcabb5132c15a71ec23
dianalynshull <dianalynshull@gmail.com>
2021-01-04 15:27:59 -0700

    created connection.js to connect to either the JAWS_DB on heroku or the local host database

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');
+
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    return;
+}
+connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'burgers_db'
+})  
+
+connection.connect(function(err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('Successfully connected to database');
+});
+
+module.exports = connection;
