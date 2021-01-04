471e98a8093e811e0b7856b0b1b95ae13c27369e
dianalynshull <dianalynshull@gmail.com>
2021-01-04 15:49:24 -0700

    added the insert function from orm to the burger model

--- models/burger.js
+++ models/burger.js
@@ -4,6 +4,11 @@ const burger = {
     all: (cb) => {
         orm.selectAll('burgers', res => {
             cb(res);
-        })
+        });
+    },
+    insert: (col, val, cb) => {
+        orm.insertOne('burgers', col, val, res => {
+            cb(res);
+        });
     }
 }
