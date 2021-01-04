3aaadc8090e4a903b799281ab2050a8017591b43
dianalynshull <dianalynshull@gmail.com>
2021-01-04 15:12:10 -0700

    initialized the repository and installed express, handlebars, and mysql. Also created/filled server file

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log("App listening on " + PORT);
+});
