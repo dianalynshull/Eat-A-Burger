61a966ce3fd7032fb8a11da0102b499e2e503797
dianalynshull <dianalynshull@gmail.com>
2021-01-04 15:16:57 -0700

    created schema file to create the database with the table of burgers

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INT, NOT NULL, AUTO_INCREMENT,
-    burger_name STRING NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(30) NOT NULL,
     devoured BOOLEAN,
     PRIMARY KEY (id)
 );
