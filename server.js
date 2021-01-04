8acf3fbb17628fa8405418a8fb77835bff45183f
dianalynshull <dianalynshull@gmail.com>
2021-01-04 16:14:56 -0700

    updated server.js to use handlebars and the routes that were created

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers/burgers_controller');
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,6 +9,11 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.engine('handlebars', exphbs({ default: 'main' }));
+app.set('view enginer', 'handlebars');
+
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log("App listening on " + PORT);
