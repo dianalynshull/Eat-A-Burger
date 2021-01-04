7f855a7738e4700d43a623346c230d96362d4597
dianalynshull <dianalynshull@gmail.com>
2021-01-04 15:39:47 -0700

    added the insert query to add a new burger to the list of burgers

--- config/orm.js
+++ config/orm.js
@@ -7,5 +7,13 @@ const orm = {
             if (err) throw err;
             cb(res);
         })
+    },
+    insertOne: (table, col, val, cb) => {
+        const query = 'INSERT INTO ' + table + ' ('+col+', devoured) VALUES (' + val + ', 0';
+        console.log(query);
+        connection.query(query, (err, res) => {
+            if (err) throw err;
+            cb(res);
+        })
     }
 }
