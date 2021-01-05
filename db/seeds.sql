9cc8547d757b35e7dcf4f01e1d4de49972b65721
dianalynshull <dianalynshull@gmail.com>
2021-01-05 00:17:11 -0700

    updated seeds to have more burgers

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', 0), ('Pub Burger', 0), ('Black Bean Burger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beets Me Burger', 0), ('To Err Is Cumin Burger', 0), ('The Grand Brie Burger', 0), ('I Heartichoke You Burger', 0), ('New Bacon-ings Burger', 0), ('Cajun Gracefully Burger', 0);
